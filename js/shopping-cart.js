c7d531eb349087eb3ed3721d4e51f8093a6eb850
JS Devware <97851374+jsdevware@users.noreply.github.com>
2022-12-07 01:21:16 +0600

    Add files via upload

---
+++ js/shopping-cart.js
@@ -0,0 +1,100 @@
+let productsInCart = JSON.parse(localStorage.getItem('shoppingCart'));
+if(!productsInCart){
+	productsInCart = [];
+}
+const parentElement = document.querySelector('#buyItems');
+const cartSumPrice = document.querySelector('#sum-prices');
+const products = document.querySelectorAll('.product-under');
+
+// this codes for add cart list
+const updateShoppingCartHTML = function () {  // 3
+	localStorage.setItem('shoppingCart', JSON.stringify(productsInCart));
+	if (productsInCart.length > 0) {
+		let result = productsInCart.map(product => {
+			return `
+				<li class="buyItem">
+					<img src="${product.image}">
+					<div class="desc">
+						<h5>${product.name}</h5>
+						<div class="counting">
+							<button class="button-minus" data-id=${product.id}>-</button>
+							<span class="countOfProduct">${product.count}</span>
+							<button class="button-plus" data-id=${product.id}>+</button>
+						</div>
+						<h6>৳${product.price}</h6>
+					</div>
+				</li>
+					`
+		});
+		parentElement.innerHTML = result.join('');
+		cartSumPrice.innerHTML = '$' + countTheSumPrice();
+
+	}
+	else {
+		parentElement.innerHTML = '<h4 class="empty">Your cart is empty </br> <a href="categories.html"><i class="bi bi-bag-heart-fill"></i> Shop Now</a></h4>';
+		cartSumPrice.innerHTML = '';
+	}
+}
+
+// this codes for add product in cart
+function updateProductsInCart(product) { // 2
+	for (let i = 0; i < productsInCart.length; i++) {
+		if (productsInCart[i].id == product.id) {
+			productsInCart[i].count += 1;
+			productsInCart[i].price = productsInCart[i].basePrice * productsInCart[i].count;
+			return;
+		}
+	}
+	productsInCart.push(product);
+}
+
+// this codes for click to add product in cart
+products.forEach(item => {   // 1
+	item.addEventListener('click', (e) => {
+		if (e.target.classList.contains('addToCart')) {
+			const productID = e.target.dataset.productId;
+			const productName = item.querySelector('.productName').innerHTML;
+			const productPrice = item.querySelector('.priceValue').innerHTML;
+			const productImage = item.querySelector('.productImg').src;
+			let product = {
+				name: productName,
+				image: productImage,
+				id: productID,
+				count: 1,
+				price: +productPrice,
+				basePrice: +productPrice,
+			}
+			updateProductsInCart(product);
+			updateShoppingCartHTML();
+		}
+	});
+});
+
+
+
+
+// this codes for increament and decreament products and prices
+parentElement.addEventListener('click', (e) => { // Last
+	const isPlusButton = e.target.classList.contains('button-plus');
+	const isMinusButton = e.target.classList.contains('button-minus');
+
+	if (isPlusButton || isMinusButton) {
+		for (let i = 0; i < productsInCart.length; i++) {
+			if (productsInCart[i].id == e.target.dataset.id) {
+				if (isPlusButton) {
+					productsInCart[i].count += 1;
+				}
+				else if (isMinusButton) {
+					productsInCart[i].count -= 1;
+				}
+				productsInCart[i].price = productsInCart[i].basePrice * productsInCart[i].count;
+			}
+			if (productsInCart[i].count <= 0) {
+				productsInCart.splice(i, 1);
+			}
+		}
+		updateShoppingCartHTML();
+	}
+});
+
+updateShoppingCartHTML();
